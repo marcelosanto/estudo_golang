76cf67c5cbc80888693768c253733efcb7575e24
Marcelo Santos <rocha.mer21@gmail.com>
2020-11-18 11:00:21 -0300

    arrays

--- 9 - Arrays e Slices/arrays-e-slices.go
+++ 9 - Arrays e Slices/arrays-e-slices.go
@@ -26,4 +26,21 @@ func main() {
 	slice2 := array2[1:3]
 	fmt.Println(slice2)
 
+	// Arrays Internos
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3, 88)
+	slice3 = append(slice3, 175)
+
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // -> length
+	fmt.Println(cap(slice3)) // -> capacidade
+
+	// quando nao se passa a capacidade, ela e automaticamente definida pelo tamanho do slice
+	slice4 := make([]float32, 5)
+	fmt.Println(slice4)
+	fmt.Println(len(slice4)) // -> length
+	fmt.Println(cap(slice4)) // -> capacidade
+
 }
