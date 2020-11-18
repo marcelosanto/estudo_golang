35554a2a1456740acf02f9fdc35d7b46f0c48d6e
Marcelo Santos <rocha.mer21@gmail.com>
2020-11-17 21:02:16 -0300

    structs

--- 6 - Structs/structs.go
+++ 6 - Structs/structs.go
@@ -3,13 +3,30 @@ package main
 import "fmt"
 
 type usuario struct {
-	nome  string
-	idade uint8
+	nome     string
+	idade    uint8
+	endereco endereco
+}
+
+type endereco struct {
+	logradouro string
+	numero     uint8
 }
 
 func main() {
 	fmt.Println("Arquivo structs")
 
 	var u usuario
+	u.nome = "Marcello"
+	u.idade = 29
 	fmt.Println(u)
+
+	enderecoExemplo := endereco{"Travessa das catatuas", 25}
+
+	usuario2 := usuario{"Marcello", 29, enderecoExemplo}
+	fmt.Println(usuario2)
+
+	usuario3 := usuario{idade: 29}
+	fmt.Println(usuario3)
+
 }
