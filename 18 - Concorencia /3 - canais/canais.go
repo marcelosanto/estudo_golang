c398b2a3039b8fb3d08416c6cd5c21141baa95f2
Marcelo Santos <rocha.mer21@gmail.com>
2020-12-01 22:17:33 -0300

    outra forma de receber mensagem usando for

--- 18 - Concorencia /3 - canais/canais.go
+++ 18 - Concorencia /3 - canais/canais.go
@@ -12,12 +12,16 @@ func main() {
 
 	fmt.Println("Depois da função escrever comecar a ser executada")
 
-	for {
-		msg, aberto := <-canal
-		if !aberto {
-			break
-		}
-		fmt.Println(msg)
+	// for {
+	// 	msg, aberto := <-canal
+	// 	if !aberto {
+	// 		break
+	// 	}
+	// 	fmt.Println(msg)
+	// }
+
+	for mensagem := range canal {
+		fmt.Println(mensagem)
 	}
 
 	fmt.Println("Fim do programa")
