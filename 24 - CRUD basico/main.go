4d0606ff15ce4160f6c335489aec6ec4c08c57d8
Marcelo Santos <rocha.mer21@gmail.com>
2021-01-30 05:02:57 -0300

    config routes for data base

--- 24 - CRUD basico/main.go
+++ 24 - CRUD basico/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crud/servidor"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,7 +12,13 @@ import (
 func main() {
 	//CRUD - CREATE - READ - UPDATE - DELETE
 
+	// CREATE - Post
+	// READER - GET
+	// UPDATE - PUT
+	// DELETE - DELETE
+
 	router := mux.NewRouter()
+	router.HandleFunc("/usuarios", servidor.CriarUsuario).Methods(http.MethodPost)
 
 	fmt.Println("Escutando na porta: 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
---
+++ 24 - CRUD basico/servidor/servidor.go
@@ -0,0 +1,7 @@
+package servidor
+
+import "net/http"
+
+func CriarUsuario(w http.Res
+	
+	
